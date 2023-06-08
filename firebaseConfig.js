c903681f80c30d9556d0d34bb75812de87ca2a48
gzdgul <gzdgul00@gmail.com>
2023-06-08 18:05:06 +0300

    new project: expo

---
+++ firebaseConfig.js
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    signInWithEmailAndPassword,
+    signOut,
+    updateProfile
+} from "firebase/auth";
+import {arrayUnion, collection, doc, getDoc, getDocs, getFirestore, setDoc, updateDoc} from "firebase/firestore";
+
+
+// Firebase yapılandırma bilgilerini buraya yapıştırın
+const firebaseConfig = {
+    apiKey: "AIzaSyB_4Rih7Vx78b5aeKO4tZ-z64Tghj5Kb7Q",
+    authDomain: "chatty-db4dc.firebaseapp.com",
+    projectId: "chatty-db4dc",
+    storageBucket: "chatty-db4dc.appspot.com",
+    messagingSenderId: "386409363832",
+    appId: "1:386409363832:web:7e6a1f737a0d0ff868f08d"
+};
+
+// Firebase'i başlatma
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+// const currentUserID = auth.currentUser.uid
+const db = getFirestore();
+
+// Yeni kullanıcı oluşturma
+export const createAccount = async (email, password) => {
+    const {user} = await createUserWithEmailAndPassword(auth, email, password)
+    // const welcome_text = 'TEST 123'
+    // // await sendBOTMessage(user.uid, welcome_text)
+    // await setBOTMessageLTS(user.uid, welcome_text)
+    return user
+}
